9c40cb4da1818dc85075e02f0314677136ca7ff7
Juan Mesaglio <juan.mesaglio@gmail.com>
2018-10-18 18:44:29 -0300

    agrego la carpeta data con el script para la creacion inicial de la tabla

---
+++ data/script_creación_inicial.sql
@@ -0,0 +1,25 @@
+/*Creacion del Modelo de Datos*/
+
+/*Creacion del esquema*/
+
+create schema PROGRAM_RANGERS (AUTHORIZATION gd ??)
+
+
+--Creacion de Funciones
+
+
+--Creacion de Procedures
+
+
+--Creacion de Tablas
+
+
+--Creo constrain para las PK
+
+
+--Creo constrain para las FK 
+
+
+--Carga inicial
+
+
