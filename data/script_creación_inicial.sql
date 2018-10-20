d0a3968732f9078a3b0e02286168ecb7c252c133
Win7-Juan <juan.mesaglio@gmail.com>
2018-10-19 22:16:01 -0300

    tablas iniciales

--- data/script_creación_inicial.sql
+++ data/script_creación_inicial.sql
@@ -2,7 +2,7 @@
 
 /*Creacion del esquema*/
 
-create schema PROGRAM_RANGERS (AUTHORIZATION gd ??)
+  create schema PROGRAM_RANGERS (AUTHORIZATION gd ??)
 
 
 --Creacion de Funciones
@@ -12,14 +12,33 @@ create schema PROGRAM_RANGERS (AUTHORIZATION gd ??)
 
 
 --Creacion de Tablas
+  create table PROGRAM_RANGERS.Rol{
+  -- id_rol int not null
+  }
 
+  create table PROGRAM_RANGER.Usuario{
+
+  }
+  create table PROGRAM_RANGER.Cliente{
+
+  }
+  create TABLE PROGRAM_RANGER.Espec_Empresa{
+
+  }
+  create table PROGRAM_RANGER.Domicilio{
+
+  }
+  create table PROGRAM_RANGER.Ubicacion{
+
+  }
+  create table PROGRAM_RANGER.Espectaculo{
+
+  }
 
 --Creo constrain para las PK
 
 
---Creo constrain para las FK 
+--Creo constrain para las FK
 
 
 --Carga inicial
-
-
