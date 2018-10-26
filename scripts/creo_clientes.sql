27a1ce9d443688680049a0837a3982dc8a81fe60
GDD <juan.mesaglio@gmail.com>
2018-10-26 10:58:56 -0300

    cambio cliente

--- scripts/creo_clientes.sql
+++ scripts/creo_clientes.sql
@@ -5,14 +5,14 @@ GO
 
 create table prueba.Cliente(
 	Cli_Dni int primary key,
-	Cli_Apellido varchar(100),
-	Cli_Nombre varchar(100),
+	Cli_Apellido varchar(255),
+	Cli_Nombre varchar(255),
 	Cli_Fecha_Nac datetime,
-	Cli_Mail varchar(100),
-	Cli_Dom_Calle varchar(30),
+	Cli_Mail varchar(255),
+	Cli_Dom_Calle varchar(255),
 	Cli_Nro_Calle int,
 	Cli_Piso int,
-	Cli_Depto char(2),
+	Cli_Depto varchar(255),
 	Cli_Nro_Postal int
 )
 
